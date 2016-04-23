9917d1248b8de870d5cff3157210cf1d09beaf50
Mathew Teoh <mteoh@live.ca>
2016-04-23 14:10:32 -0700

    Revert "Revert "small change in body""
    This reverts commit 02ace022b2f482b2a42c6d8e1329c872c0bf2839.

--- controller/visualization.py
+++ controller/visualization.py
@@ -5,4 +5,4 @@ import falcon
 class VisualizationPage(object):
     def on_get(self,req,resp):
         resp.status = falcon.HTTP_200
-        resp.body = ('\nYou have landed on the viz page!\n')
+        resp.body = 'You have landed on the viz page!'
