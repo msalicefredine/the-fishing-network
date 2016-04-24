8f4eed305a2e4ae243ddac318911baded3c250c9
anyamcgee <anyamcgee@gmail.com>
2016-04-23 22:05:27 -0700

    Added more styling to views

--- controller/view_datasets.py
+++ controller/view_datasets.py
@@ -1,4 +1,6 @@
 import falcon
+from pymongo import MongoClient
+import json
 
 class ViewDatasetsPage(object):
     file_count = 0
@@ -6,6 +8,16 @@ class ViewDatasetsPage(object):
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200  # This is the default status
 
-        page = open('views/view_datasets.html', 'r')
-        resp.stream = page
-        resp.content_type = "text/html"
+        if req.params.get("get_tables", None) is not None:
+            resp.data = self.get_tables()
+            resp.content_type = "text/json"
+        else:
+            page = open('views/view_datasets.html', 'r')
+            resp.stream = page
+            resp.content_type = "text/html"
+
+    def get_tables(self):
+        client = MongoClient("localhost:27017")
+        db = client.fishing_network
+        names = db.collection_names(include_system_collections = False)
+        return json.dumps(names)
